dbce6bf75539790cb8b08b7508cb8c18a7ae7126
Clyde Mendez <cmende94@my.epcc.edu>
2026-05-04 14:08:33 -0600

    Add starter code to Game.java

--- In Class Lab Composition/Game.java
+++ In Class Lab Composition/Game.java
@@ -1,3 +1,9 @@
 public class Game {
-    
+    public static void main(String[] args) {
+        Weapon sword = new Weapon("Sword", 15, 3.5);
+        Weapon bow = new Weapon("Bow", 10, 1.5);
+ 
+        System.out.println(sword);
+        System.out.println(bow);
+    }
 }
