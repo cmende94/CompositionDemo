1a23e2085635b5703eb43485b95007a2e8ebce8b
Clyde Mendez <cmende94@my.epcc.edu>
2026-05-04 14:12:12 -0600

    Complete weapon class

--- In Class Lab Composition/Weapon.java
+++ In Class Lab Composition/Weapon.java
@@ -1,3 +1,20 @@
 public class Weapon {
-    
+    private String name;
+    private int damage;
+    private double weight;
+
+    public String getName() {return name;}
+    public int getDamage() {return damage;}
+    public double getWeight() {return weight;}
+
+    Weapon(String name, int damage, double weight){
+        this.name = name;
+        this.damage = damage;
+        this.weight = weight;
+    }
+
+    public String toString(){
+        return name + ": " + damage + "dmg, " + weight + "lbs";
+    }
+
 }
