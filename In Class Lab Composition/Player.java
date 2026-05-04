b2b0b0ea1132da3efa10012851de6b1126d335dd
Clyde Mendez <cmende94@my.epcc.edu>
2026-05-04 14:37:03 -0600

    Complete Player

--- In Class Lab Composition/Player.java
+++ In Class Lab Composition/Player.java
@@ -1,3 +1,23 @@
 public class Player {
-    
+    private String name;
+    private int health;
+    private Inventory bag;
+
+    Player(String name, int health, int capacity){
+        this.name = name;
+        this.health = health;
+        bag = new Inventory(capacity);
+    }
+
+    public void pickUp(Weapon w){
+        bag.addWeapon(w);
+        System.out.println(name + " picked up " + w.getName());
+    }
+
+    public Inventory getBag(){return bag;}
+
+    public String toString(){
+        return name + " (HP: " + health + ") " + "carrying " + bag.getCount() + " items.";
+    }
+
 }
