846daf6efe9828d4d0f76c5e0190f225f52edab5
Clyde Mendez <cmende94@my.epcc.edu>
2026-05-04 14:30:39 -0600

    Complete inventory

--- In Class Lab Composition/Inventory.java
+++ In Class Lab Composition/Inventory.java
@@ -1,3 +1,37 @@
 public class Inventory {
-    
+    private int count;
+    private Weapon[] weapons;
+    Inventory(int size){
+        count = 0;
+        weapons = new Weapon[size];
+    }
+
+    public void addWeapon(Weapon w){
+        if(count < weapons.length){
+            weapons[count] = w;
+            count++;
+        }
+        else{
+            System.out.println("Inventory full");
+        }
+    }
+
+    public int getCount(){return count;}
+
+    public double totalWeight(){
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum += weapons[i].getWeight();
+        }
+        return sum;
+    }
+
+    public String toString(){
+        String returnString = "";
+        for (int i = 0; i < count; i++) {
+            returnString += weapons[i] + "\n";
+        }
+        return returnString;
+    }
+
 }
