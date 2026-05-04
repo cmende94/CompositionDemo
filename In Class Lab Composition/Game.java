2aff3588a04c80e9dc77cecef502b6157ac9a632
Clyde Mendez <cmende94@my.epcc.edu>
2026-05-04 14:31:27 -0600

    Replaced main starter code

--- In Class Lab Composition/Game.java
+++ In Class Lab Composition/Game.java
@@ -2,16 +2,19 @@ public class Game {
     public static void main(String[] args) {
         Weapon sword = new Weapon("Sword", 15, 3.5);
         Weapon bow = new Weapon("Bow", 10, 1.5);
- 
-        System.out.println(sword);
-        System.out.println(bow);
-
-        Inventory bag = new Inventory(3);
-        bag.addWeapon(sword);
-        bag.addWeapon(bow);
-        
-        System.out.println("Items in bag: " + bag.getCount());
-        System.out.println("Total weight: " + bag.totalWeight() + " lbs");
-        System.out.println(bag);
+        Weapon dagger = new Weapon("Dagger", 5, 0.5);
+         
+        Player hero = new Player("Hero", 100, 5);
+         
+        hero.pickUp(sword);
+        hero.pickUp(bow);
+        hero.pickUp(dagger);
+         
+        System.out.println();
+        System.out.println(hero);
+        System.out.println("Total weapon weight: " + hero.getBag().totalWeight() + " lbs");
+        System.out.println();
+        System.out.println("--- Loadout ---");
+        System.out.println(hero.getBag());
     }
 }
