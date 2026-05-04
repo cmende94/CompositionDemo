12fd2535ec2499c58672f1acbe1eafc962b11232
Clyde Mendez <cmende94@my.epcc.edu>
2026-05-04 13:31:08 -0600

    Print different strings

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Date today = new Date(2026, 5, 4);
         System.out.println(today.toString());
+        System.out.println(today.toISOString());
+        System.out.println(today.toMXString());
     }
 }
